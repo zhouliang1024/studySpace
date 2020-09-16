8f1548693986419ba162f97eac22386eb7e66645
yuye <zhouliang654@163.com>
2020-09-16 23:52:14 +0800

    first commit

---
+++ src/main/java/com/yuye/light/LightApplication.java
@@ -0,0 +1,13 @@
+package com.yuye.light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuye/light/LightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuye.light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
